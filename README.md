6ca4e91b51e5f9fc3c62eea7c1ff6406d57c10a0
Yash-Singh1 <saiansh2525@gmail.com>
2021-06-04 21:46:01 -0700

    Init

---
+++ README.md
@@ -0,0 +1,21 @@
+# `nolint-cli`
+
+CLI for [`nocodelint`](https://github.com/Yash-Singh1/nocodelint).
+
+## CLI
+
+Install:
+
+```sh
+npm install --global nolint-cli
+```
+
+Using:
+
+```sh
+# Read the man page
+man nolint
+
+# OR Look at the help output
+nolint --help
+```
---
+++ cli.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const nocodelint = require('nocodelint');
+const fs = require('fs');
+const chalk = require('chalk');
+const meow = require('meow');
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+
+updateNotifier({ pkg }).notify();
+
+const cli = meow(
+  `
+Usage
+  $ nolint <file>
+
+Options
+  <file> The file to be linted
+
+Examples
+  $ nolint hello.no  
+
+  $ nolint index.no
+    ✘ index.no isn't empty
+
+  $ nolint
+    ✘ At least one argument must be present
+
+  $ nolint notthere.no
+    ✘ notthere.no doesn't exist
+
+  $ nolint something
+    ✘ nocode files must have the .no file extension
+`
+);
+
+if (cli.input.length === 0) {
+  console.log(`${chalk.red('✘')} At least one argument must be present`);
+  process.exit(1);
+} else if (!cli.input[0].endsWith('.no') || !fs.existsSync(cli.input[0])) {
+  if (!cli.input[0].endsWith('.no')) {
+    console.log(
+      `${chalk.red('✘')} nocode files must have the .no file extension`
+    );
+  }
+  if (!fs.existsSync(cli.input[0])) {
+    console.log(`${chalk.red('✘')} ${cli.input[0]} doesn't exist`);
+  }
+  process.exit(1);
+} else {
+  const result = nocodelint(fs.readFileSync(cli.input[0], 'utf-8'));
+  if (!result) {
+    console.log(`${chalk.red('✘')} ${cli.input[0]} isn't empty`);
+  }
+}
